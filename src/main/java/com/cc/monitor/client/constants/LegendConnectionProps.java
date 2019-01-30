6610c683bf8f99fba0eccb4144b9ae2087a014dc
lishicheng05 <lishicheng05@meituan.com>
2019-01-31 00:02:11 +0800

    init project

---
+++ src/main/java/com/cc/monitor/client/constants/LegendConnectionProps.java
@@ -0,0 +1,7 @@
+package com.cc.monitor.client.constants;
+
+public class LegendConnectionProps {
+
+    public static final int SOCKET_TIMEOUT = 3000;
+    public static final int CONN_TIMEOUT = 5000;
+}
