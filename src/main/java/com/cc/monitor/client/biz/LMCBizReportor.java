96511807599f2de4f0e930973997347d58d3fa2d
lishicheng05 <lishicheng05@meituan.com>
2019-03-02 09:37:33 +0800

    commit

--- src/main/java/com/cc/monitor/client/biz/LMCBizReportor.java
+++ src/main/java/com/cc/monitor/client/biz/LMCBizReportor.java
@@ -1,5 +1,6 @@
 package com.cc.monitor.client.biz;
 
+import com.cc.monitor.client.constants.LMCConstants;
 import com.cc.monitor.client.util.HttpClient;
 
 import java.io.IOException;
@@ -15,9 +16,14 @@ public class LMCBizReportor {
 
     public static void reportBizQuota(String quotaKey) {
         Map<String,Object> params = new HashMap<>();
-        params.put("device" , DEVICE_NAME);
-        params.put("app" , APP_NAME);
-        params.put(quotaKey, 1);
+        params.put("device.name", LMCConstants.DEVICE_NAME);
+        params.put("device.key", LMCConstants.DEVICE_KEY);
+        params.put("app.name", LMCConstants.APP_NAME);
+
+        Map<String,Object> contentMap = new HashMap<>();
+        contentMap.put(quotaKey, 1);
+
+        params.put("quotas", contentMap);
 
         try {
             HttpClient.post(BIZ_NORMAL_URL, params);
@@ -28,9 +34,32 @@ public class LMCBizReportor {
 
     public static void reportBizQuota(String quotaKey, long value) {
         Map<String,Object> params = new HashMap<>();
-        params.put("device" , DEVICE_NAME);
-        params.put("app" , APP_NAME);
-        params.put(quotaKey, value);
+        params.put("device.name", LMCConstants.DEVICE_NAME);
+        params.put("device.key", LMCConstants.DEVICE_KEY);
+        params.put("app.name", LMCConstants.APP_NAME);
+
+        Map<String,Object> contentMap = new HashMap<>();
+        contentMap.put(quotaKey, value);
+
+        params.put("quotas", contentMap);
+
+        try {
+            HttpClient.post(BIZ_NORMAL_URL, params);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void reportBizQuota(Map<String,Long> quotaMap) {
+        Map<String,Object> params = new HashMap<>();
+        params.put("device.name", LMCConstants.DEVICE_NAME);
+        params.put("device.key", LMCConstants.DEVICE_KEY);
+        params.put("app.name", LMCConstants.APP_NAME);
+
+        Map<String,Object> contentMap = new HashMap<>();
+        contentMap.putAll(quotaMap);
+
+        params.put("quotas", contentMap);
 
         try {
             HttpClient.post(BIZ_NORMAL_URL, params);
